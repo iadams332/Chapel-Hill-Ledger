05ffa3168cbcd2f327ef19ca34358faa37dd60ba
Isaac Adams <isaac.adams332@gmail.com>
2020-12-01 13:09:07 -0500

    Initial Commit

---
+++ Chapel Hill Ledger/Chapel_Hill_LedgerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chapel_Hill_LedgerApp.swift
+//  Chapel Hill Ledger
+//
+//  Created by Isaac Adams on 12/1/20.
+//
+
+import SwiftUI
+
+@main
+struct Chapel_Hill_LedgerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
